ee56558044e63d22f7c0aa44b13e325da2716ec4
CMLTTN <cemaldim@gmail.com>
2023-04-08 14:50:27 +0300

    button

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const input = document.getElementById("input");
-const button =document.querySelectorAll("#button");
-const attempt = document.querySelector(".attempt");
-const refresh = document.getElementById("refresh");
-const randomNum = Math.round(Math.randoom()*100);
+let input = document.getElementById("number");
+let check = document.querySelector(".check");
+let info = document.querySelector(".info");
+let attempt = document.querySelector(".attempt");
+let downNum = document.querySelector(".downNum")  
+let upNum = document.querySelector(".upNum")
 
-let counter = 0;
+let counter = 9;
+
+check.addEventListener("click", ()=>{
+     counter--;
+     attempt.innerHTML= `Number 0f attempts: ${counter}`;
+});
+ 
+let randomNumber = Math.round(Math.random()*100);
+
+input.addEventListener("click", ()=>{
+     input.value = "";
+});
+
+input.addEventListener("keyup", ())
