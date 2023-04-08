7d9b90e5a941a94435ba8731812f1a025da60d55
CMLTTN <cemaldim@gmail.com>
2023-04-08 13:30:11 +0300

    refresh

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const input = document.getElementById("input")
-const button =document.querySelectorAll("#button")
-const attempt = document.querySelector(".attempt")
+const input = document.getElementById("input");
+const button =document.querySelectorAll("#button");
+const attempt = document.querySelector(".attempt");
+const refresh = document.getElementById("refresh");
+const randomNum = Math.round(Math.randoom()*100);
+
+let counter = 0;
